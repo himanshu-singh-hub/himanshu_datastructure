715ff6faf185d33f69924b78f9688f831fc6ddc0
himanshu singh <76714570+himanshu-singh-hub@users.noreply.github.com>
2022-07-31 21:30:42 +0530

    Update First and Last Position of an Element In Sorted Array.cpp

--- First and Last Position of an Element In Sorted Array.cpp
+++ First and Last Position of an Element In Sorted Array.cpp
@@ -1,103 +1,51 @@
 int firstOcc(vector<int>& arr, int n, int key)
 
 {
-
    int s = 0, e = n - 1;
-
    int mid = s + (e - s) / 2;
-
    int ans = -1;
-
- 
-
    while (s <= e)
-
    {
-
        if (arr[mid] == key)
-
        {
-
            ans = mid;
-
            e = mid - 1; // this is done to find the leftmost occurence!!
-
        }
-
        else if (arr[mid] < key)
-
        {
-
            s = mid + 1;
-
        }
-
        else if (arr[mid] > key)
-
        {
-
            e = mid - 1;
-
        }
-
        mid = s + (e - s) / 2;
-
    }
-
    return ans;
-
 }
-
- 
-
 int lastOcc(vector<int>& arr, int n, int key)
-
 {
-
    int s = 0, e = n - 1;
-
    int mid = s + (e - s) / 2;
-
    int ans = -1;
-
- 
-
    while (s <= e)
-
    {
-
        if (arr[mid] == key)
-
        {
-
            ans = mid;
-
            s = mid + 1;
-
        }
-
        else if (arr[mid] < key)
-
        {
-
            s = mid + 1;
-
        }
-
        else if (arr[mid] > key)
-
        {
-
            e = mid - 1;
-
        }
-
        mid = s + (e - s) / 2;
-
    }
-
    return ans;
-
 }
 
  
